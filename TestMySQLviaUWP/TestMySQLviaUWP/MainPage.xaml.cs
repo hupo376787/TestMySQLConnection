b3d1aa973507f74426c743d6dd6e2d604ed151a3
Vincent Wang <376787823@qq.com>
2018-01-17 01:57:47 -0600

    Add files via upload

---
+++ TestMySQLviaUWP/TestMySQLviaUWP/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace TestMySQLviaUWP
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+
+        private static string strServer = "testconnect.c7bynrothlke.us-west-1.rds.amazonaws.com";
+        private static string strUserID = "test";
+        private static string strPassword = "test123456789";
+        private static string strDatabase = "test";
+        private static string strPort = "3306";
+
+        private static MySqlConnection MySqlConn;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MySqlConn = CreateMySqlConnection();
+        }
+
+        public MySqlConnection CreateMySqlConnection()
+        {
+            string strConn = String.Format("server={0}; uid={1}; pwd={2}; database={3}",
+                strServer, strUserID, strPassword, strDatabase);
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection();
+                conn.ConnectionString = strConn;
+                conn.Open();
+            }
+            catch (MySqlException myex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return conn;
+        }
+    }
+}
---
+++ TestMySQLviaWinForm/TestMySQLviaWinForm/Form1.cs
@@ -0,0 +1,56 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestMySQLviaWinForm
+{
+    public partial class Form1 : Form
+    {
+        private string strServer = "testconnect.c7bynrothlke.us-west-1.rds.amazonaws.com";
+        private string strUserID = "test";
+        private string strPassword = "test123456789";
+        private string strDatabase = "test";
+        private string strPort = "3306";
+
+        private static MySqlConnection MySqlConn;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConn = CreateMySqlConnection();
+        }
+
+        public MySqlConnection CreateMySqlConnection()
+        {
+            string strConn = String.Format("server={0}; uid={1}; pwd={2}; database={3}; port={4}",
+                strServer, strUserID, strPassword, strDatabase, strPort);
+            MySqlConnection conn = null;
+            try
+            {
+                conn = new MySqlConnection(strConn);
+                conn.Open();
+            }
+            catch (MySqlException myex)
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return conn;
+        }
+    }
+}
